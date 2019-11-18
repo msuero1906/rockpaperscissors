2b89d0107aa17f6d2c2a9c44503b7bb136976cf5
msuero1906 <msuero1906.student@hsfi.us>
2019-11-18 23:07:50 +0000

    conditional statements

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 // *************************** YOUR CODE BELOW *******************************
 $("#shoot").click(function() {
+let result = $('#input').val();
+$('#userChoice').append(result)
 
-  let result = $('#input').val();
+var rn= Math.random();
 
-  $('#userChoice').text(result)
+if ( rn < 0.3  && rn >= 0 ){
+$('#computerChoice').append('rock')
+ 
+} 
+
+else if (rn <=0.70  && rn >=0.3  ) {
+$('#computerChoice').append('paper')
+
+}
+
+else if (rn <=1 && rn > 0.70 ) {
+$('#computerChoice').append('scissors')
+
+}
 });
 
 //******************TEST EARLY AND OFTEN USING console.log() ******************
