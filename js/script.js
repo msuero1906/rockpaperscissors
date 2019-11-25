809a756ee42a3edb28391e8d481b06513a62d30a
msuero1906 <msuero1906.student@hsfi.us>
2019-11-25 23:04:43 +0000

    lol

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
 // *************************** YOUR CODE BELOW *******************************
 $("#shoot").click(function() {
 let result = $('#input').val();
-$('#userChoice').append(result)
+$('#userChoice').text(result)
 
 var rn= Math.random();
 
 if ( rn < 0.3  && rn >= 0 ){
-$('#computerChoice').append('rock')
+$('#computerChoice').text('rock')
  
 } 
 
 else if (rn <=0.70  && rn >=0.3  ) {
-$('#computerChoice').append('paper')
+$('#computerChoice').text('paper')
 
 }
 
 else if (rn <=1 && rn > 0.70 ) {
-$('#computerChoice').append('scissors')
-
+$('#computerChoice').text('scissors')
 }
+   // var result 
+
+   // if ( result = '#userchoice' )
 });
 
 //******************TEST EARLY AND OFTEN USING console.log() ******************
