20e75b2caa48fa5ffba9af4ec9cd2aa1fb0f620d
msuero1906 <msuero1906.student@hsfi.us>
2019-11-29 23:04:58 +0000

    i finished

--- js/script.js
+++ js/script.js
@@ -5,22 +5,64 @@ $('#userChoice').text(result)
 
 var rn= Math.random();
 
-if ( rn < 0.3  && rn >= 0 ){
-$('#computerChoice').text('rock')
- 
+    if ( rn < 0.3  && rn >= 0 ){
+        $('#computerChoice').text('rock')
+        var computerChoice1 = "rock"; 
 } 
 
-else if (rn <=0.70  && rn >=0.3  ) {
-$('#computerChoice').text('paper')
-
+    else if (rn <=0.70  && rn >=0.3  ) {
+        $('#computerChoice').text('paper')
+        var computerChoice2 = "paper";
 }
 
-else if (rn <=1 && rn > 0.70 ) {
-$('#computerChoice').text('scissors')
+    else if (rn <=1 && rn > 0.70 ) {
+        $('#computerChoice').text('scissors')
+        var computerChoice3 = "scissors";
 }
-   // var result 
 
-   // if ( result = '#userchoice' )
+   // ***TIES***
+
+    if((result === "rock") && (computerChoice1 === "rock")){
+        $("#result").text("TIE!!!");
+    }
+
+    else if((result === "paper") && (computerChoice2 === "paper")){
+        $("#result").text("TIE!!!");
+    }
+
+    else if((result === "scissors") && (computerChoice3 === "scissors")){
+        $("#result").text("TIE!!!");
+    }
+
+    // ***COMPUTER WINS***
+
+    else if((result === "rock") && (computerChoice2 === "paper")){
+        $("#result").text("COMPUTER WINS!!!");
+    }
+
+    else if((result === "paper") && (computerChoice3 === "scissors")){
+        $("#result").text("COMPUTER WINS!!!");
+    }
+
+    else if((result === "scissors") && (computerChoice1 === "rock")){
+        $("#result").text("COMPUTER WINS!!!");
+    }
+
+    // ***YOU WIN***
+
+    else if ((result === "paper") && (computerChoice1 === "rock")){
+        $("#result").text("YOU WIN!!!");
+    }
+
+    else if ((result === "scissors") && (computerChoice2 === "paper")){
+        $("#result").text("YOU WIN!!!");
+    }
+
+    else if ((result === "rock") && (computerChoice3 === "scissors")){
+        $("#result").text("YOU WIN!!!")
+    }
+    
+
 });
 
 //******************TEST EARLY AND OFTEN USING console.log() ******************
